21a18ddcc0452ac74ac1f4d429a952399566cd86
Mihaela Martinas <martinas.mihaela21@gmail.com>
2013-03-30 20:35:34 +0200

    Interfata log-in
    Interfata este un dummy, fara sa faca nimic in afara de afisare.
    
    Signed-off-by: Mihaela Martinas <martinas.mihaela21@gmail.com>

--- src/gui/IGUI.java
+++ src/gui/IGUI.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 import mediator.IMediatorGUI;
 
-public abstract class IGUI {
+public interface IGUI {
 	
-	public IMediatorGUI medGUI;
-
-	public abstract void updateServices(List<String> offers);
-	public abstract void interruptTransfer(String seller, String serviceName);
+	public void updateServices(List<String> offers);
+	public void interruptTransfer(String seller, String serviceName);
 }
