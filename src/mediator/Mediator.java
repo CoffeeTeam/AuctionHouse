ba0422f019adfc3a85a721388a8ed069e2f38905
Tommy <cristina.mihaela.ciocan@gmail.com>
2013-04-01 23:32:44 +0300

    Mediator merge - accept offer

--- src/mediator/Mediator.java
+++ src/mediator/Mediator.java
@@ -79,8 +79,20 @@ public class Mediator implements IMediatorGUI, IMediatorNetwork,
 
 	@Override
 	public void acceptOfferNet(String seller, String offer) {
-		// TODO Auto-generated method stub
-		network.acceptOffer(seller, offer);
+		// TODO
+
+		// Refuse other offers
+		List<String> otherOffers = stateMgr.getCurrentUsers(offer);
+		for (String otherOff:otherOffers) {
+			if (otherOff.equals(offer))
+				continue;
+			
+			// TODO change status of offer to "Offer Refused"
+			
+		}
+		
+		// Accept selected offer and start the transfer
+		// TODO mark offer as accepted
 	}
 
 	@Override
