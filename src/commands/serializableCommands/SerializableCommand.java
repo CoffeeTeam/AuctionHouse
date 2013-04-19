1873c2a5f114dbbb185c72d8ac84e21cfab440aa
Tommy <cristina.mihaela.ciocan@gmail.com>
2013-04-19 23:14:49 +0300

    Member update for a serializable command

--- src/commands/serializableCommands/SerializableCommand.java
+++ src/commands/serializableCommands/SerializableCommand.java
@@ -1,6 +1,8 @@
 package commands.serializableCommands;
 
 import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
 
 import mediator.IMediatorNetwork;
 
@@ -9,6 +11,12 @@ public abstract class SerializableCommand implements Serializable{
 	private static final long serialVersionUID = 612696483138661950L;
 
 	public transient IMediatorNetwork medNetwork;
-	public String username;
+	
+	public SerializableCommand() {
+		commandInfo = new LinkedList<String>();
+	}
+
+	public String userName;
 	public String serviceName;
+	public List<String> commandInfo;
 }
