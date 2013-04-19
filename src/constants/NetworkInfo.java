ef1b0f3ca6eb71b62415ea9a3cd7d23f61ce83e4
Mihaela Martinas <martinas.mihaela21@gmail.com>
2013-04-19 16:43:42 +0300

    Network Constants
    Signed-off-by: Mihaela Martinas <martinas.mihaela21@gmail.com>

---
+++ src/constants/NetworkInfo.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class NetworkInfo {
+
+	public static final String IP		= "127.0.0.1";
+	public static final int PORT 		= 30001;
+	public static final int BUF_SIZE	= 4;
+}
