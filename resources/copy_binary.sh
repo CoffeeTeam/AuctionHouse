a2a4472f4fd8733c7009e18bedeb7612fbdc3d2e
Martinas Mihaela <martinas.mihaela21@gmail.com>
2013-05-10 21:13:16 +0300

    Useful folders for compiling using ant
    Signed-off-by: Martinas Mihaela <martinas.mihaela21@gmail.com>

---
+++ resources/copy_binary.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp -rf ../bin/*	../../apache-tomcat/webapps/axis/WEB-INF/classes
---
+++ resources/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+java -cp $AXISCLASSPATH org.apache.axis.client.AdminClient -lhttp://localhost:8080/axis/services/AdminService deploy.wsdd
+
